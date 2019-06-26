a0a74b851cacf62e42ae6463ca8a6311ae7d4f57
Emma <e.m.bunsness@gmail.com>
2019-06-25 20:50:46 -0600

    schema.sql updated with create database

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,29 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS product_db;
+CREATE DATABASE product_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE product_db;
+
+CREATE TABLE productTable {
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    productDepartment VARCHAR(50) NOT NULL,
+    productCategory VARCHAR(10) NOT NULL,
+    productName VARCHAR(50) NOT NULL,
+    productInventory INTEGER (50) NOT NULL,
+    productPrice DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (id)  
+}
+
+INSERT INTO productTable (productDepartment, productCategory, productName, productInventory, productPrice)
+VALUES ("Cosmetics", "Mascara", "Covergirl", 22, 15.95);
+
+INSERT INTO productTable (productDepartment, productCategory, productName, productInventory, productPrice)
+VALUES ("Hair Care", "Shampoo", "Pantene", 03, 8.95);
+
+INSERT INTO productTable (productDepartment, productCategory, productName, productInventory, productPrice)
+VALUES ("Hair Care", "Conditioner", "Pantene", 08, 10.95);
+
+SELECT * FROM productTable;
+
+
+--DROP DATABASE IF EXISTS testdb;
+--CREATE DATABASE testdb;
